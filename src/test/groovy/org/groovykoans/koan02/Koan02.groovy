7050d40b2ad3f748cb113fb589793d85527e2ca9
sskalicky <skalicky.s@gmail.com>
2013-06-19 00:03:33 +0200

    Koan02 - tests have been made pass

--- src/test/groovy/org/groovykoans/koan02/Koan02.groovy
+++ src/test/groovy/org/groovykoans/koan02/Koan02.groovy
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
- 
+
 package org.groovykoans.koan02
 
 /**
@@ -23,69 +23,69 @@ package org.groovykoans.koan02
  */
 class Koan02 extends GroovyTestCase {
 
-    void test01_RegularBooleanExpressions() {
-        // Boolean expressions work as expected. true evaluates to true and false to false.
-        def predicate1 = false
-        def predicate2 = true
+	void test01_RegularBooleanExpressions() {
+		// Boolean expressions work as expected. true evaluates to true and false to false.
+		def predicate1 = false
+		def predicate2 = true
 
-        // ------------ START EDITING HERE ----------------------
+		// ------------ START EDITING HERE ----------------------
+		predicate1 = true
+		predicate2 = false
 
+		// ------------ STOP EDITING HERE  ----------------------
 
-        // ------------ STOP EDITING HERE  ----------------------
+		assert predicate1
+		assert predicate2 == false
+	}
 
-        assert predicate1
-        assert predicate2 == false
-    }
+	void test02_Collections() {
+		// Lists and map evaluate to false if they're empty. Otherwise, they evaluate to true
+		Map<String, String> map = [:]
+		List<String> list = ['item']
 
-    void test02_Collections() {
-        // Lists and map evaluate to false if they're empty. Otherwise, they evaluate to true
-        Map<String, String> map = [:]
-        List<String> list = ['item']
+		// ------------ START EDITING HERE ----------------------
+		map.put("key", "value")
+		list.clear()
+		// ------------ STOP EDITING HERE  ----------------------
 
-        // ------------ START EDITING HERE ----------------------
+		assert map.asBoolean()
+		assert list.asBoolean() == false
+	}
 
+	void test03_StringTruth() {
+		// Quite intuitively, empty (or null) strings are false.
+		String s1 = 'Non-empty string'
+		String s2 = ''
 
-        // ------------ STOP EDITING HERE  ----------------------
+		// ------------ START EDITING HERE ----------------------
+		s1 = ''
+		s2 = 'ne'
+		// ------------ STOP EDITING HERE  ----------------------
 
-        assert map.asBoolean()
-        assert list.asBoolean() == false
-    }
+		// Note how we can use String expressions inside an if statement.  It is automatically converted into a boolean.
+		if (s1) {
+			fail()
+		}
+		assert s2.asBoolean()
+	}
 
-    void test03_StringTruth() {
-        // Quite intuitively, empty (or null) strings are false.
-        String s1 = 'Non-empty string'
-        String s2 = ''
+	void test04_NumericTruth() {
+		// Similar to C code, null or zeros are false. Any other number is true.
+		def balance = [2, -3, 6, 0, 5]
 
-        // ------------ START EDITING HERE ----------------------
+		// Remove (or change) the offending integer to continue
+		// ------------ START EDITING HERE ----------------------
+		balance[3] = 8
+		// ------------ STOP EDITING HERE  ----------------------
 
-
-        // ------------ STOP EDITING HERE  ----------------------
-
-        // Note how we can use String expressions inside an if statement.  It is automatically converted into a boolean.
-        if (s1) {
-            fail()
-        }
-        assert s2.asBoolean()
-    }
-
-    void test04_NumericTruth() {
-        // Similar to C code, null or zeros are false. Any other number is true.
-        def balance = [ 2, -3, 6, 0, 5 ]
-
-        // Remove (or change) the offending integer to continue
-        // ------------ START EDITING HERE ----------------------
-
-
-        // ------------ STOP EDITING HERE  ----------------------
-
-        // Iterate through the list and AND the boolean values of its members.
-        // For an easier way to do this in Groovy, check out the any() method at
-        // http://groovy.codehaus.org/groovy-jdk/java/lang/Object.html#any(groovy.lang.Closure)
-        def result = true
-        for (int i : balance) {
-            result = result && i.asBoolean()
-        }
-        assert result
-    }
+		// Iterate through the list and AND the boolean values of its members.
+		// For an easier way to do this in Groovy, check out the any() method at
+		// http://groovy.codehaus.org/groovy-jdk/java/lang/Object.html#any(groovy.lang.Closure)
+		def result = true
+		for (int i : balance) {
+			result = result && i.asBoolean()
+		}
+		assert result
+	}
 
 }
